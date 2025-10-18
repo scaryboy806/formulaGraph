a0544b1eb752d43461c5eda9f5d7ee2a5389119c
Izak <izakfabjan5@gmail.com>
2025-10-19 01:33:41 +0200

    Adds get_race_results function

--- main.py
+++ main.py
@@ -13,8 +13,19 @@ def get_last_race() -> list[str]:
     data = response.json()["race"][0]
     return [data["championshipId"], str(data["round"])]
 
+def get_race_results(championship_id: str, round: str, driver_id: str) -> int:
+    year = championship_id[-4:]
+    response = requests.get(f"https://f1api.dev/api/{year}/{round}/race")
+    data = response.json()["races"]["results"]
+    for result in data:
+        if result["driver"]["driverId"] == driver_id:
+            return result["points"]
+    return -1
+
 def main():
     print("Hello from formulagraph!")
+    lastRace: list[str] = get_last_race()
+    print(get_race_results(lastRace[0], str(int(lastRace[1]) - 1), "sainz"))
 
 
 if __name__ == "__main__":
