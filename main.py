0332db261fd59360c64c068016c8ddb881b0c1c8
Izak <izakfabjan5@gmail.com>
2025-10-19 00:55:50 +0200

    Adds requests dependency and creates a get_drivers function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import requests
+
+def get_drivers() -> dict[str, list[str | list[int]]]:
+    response = requests.get("https://f1api.dev/api/current/drivers")
+    data = response.json()["drivers"]
+    drivers = {}
+    for driver in data:
+        drivers[driver["driverId"]] = [driver["shortName"], []]
+    return drivers
+
 def main():
     print("Hello from formulagraph!")
 
