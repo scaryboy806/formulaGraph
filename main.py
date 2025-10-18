6e2f53302b17f947be19b5cbce1d76d419874689
Izak <izakfabjan5@gmail.com>
2025-10-19 01:10:28 +0200

    Adds get_last_race function

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def get_drivers() -> dict[str, list[str | list[int]]]:
         drivers[driver["driverId"]] = [driver["shortName"], []]
     return drivers
 
+def get_last_race() -> list[str]:
+    response = requests.get("https://f1api.dev/api/current/last")
+    data = response.json()["race"][0]
+    return [data["championshipId"], str(data["round"])]
+
 def main():
     print("Hello from formulagraph!")
 
