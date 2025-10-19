c469c08b959ff40a9702ea49a092cacb4d18d230
Izak <izakfabjan5@gmail.com>
2025-10-19 13:16:02 +0200

    Updates formatting and adds sprint results

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 import requests
 
+
 def get_drivers() -> dict[str, list[str | list[int]]]:
-    response = requests.get("https://f1api.dev/api/current/drivers")
+    response: requests.Response = requests.get("https://f1api.dev/api/current/drivers")
     data = response.json()["drivers"]
-    drivers = {}
+    drivers: dict[str, list[str | list[int]]] = {}
     for driver in data:
         drivers[driver["driverId"]] = [driver["shortName"], []]
     return drivers
 
+
 def get_last_race() -> list[str]:
-    response = requests.get("https://f1api.dev/api/current/last")
+    response: requests.Response = requests.get("https://f1api.dev/api/current/last")
     data = response.json()["race"][0]
     return [data["championshipId"], str(data["round"])]
 
+
 def get_race_results(championship_id: str, round: str, driver_id: str) -> int:
-    year = championship_id[-4:]
-    response = requests.get(f"https://f1api.dev/api/{year}/{round}/race")
+    year: str = championship_id[-4:]
+    response: requests.Response = requests.get(
+        f"https://f1api.dev/api/{year}/{round}/race"
+    )
     data = response.json()["races"]["results"]
+    points: int = 0
     for result in data:
         if result["driver"]["driverId"] == driver_id:
-            return result["points"]
-    return -1
+            points += result["points"]
+
+    response = requests.get(f"https://f1api.dev/api/{year}/{round}/sprint/race")
+    data = response.json()["races"]["sprintRaceResults"]
+
+    for result in data:
+        if result["driver"]["driverId"] == driver_id:
+            points += result["points"]
+
+    return points
+
 
 def main():
     print("Hello from formulagraph!")
     lastRace: list[str] = get_last_race()
-    print(get_race_results(lastRace[0], str(int(lastRace[1]) - 1), "sainz"))
+    print(get_race_results(lastRace[0], "2", "piastri"))
 
 
 if __name__ == "__main__":
